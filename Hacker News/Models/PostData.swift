fbb2a17468dffa3b37870e9d1f29ac6b3c1f18ab
Ata <atakanozalan@gmail.com>
2023-06-02 16:06:32 +0300

    Networking

---
+++ Hacker News/Models/PostData.swift
@@ -0,0 +1,23 @@
+//
+//  PostData.swift
+//  Hacker News
+//
+//  Created by Ezagor on 2.06.2023.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String{
+        return objectID
+    }
+    
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
